2aab815c79b1dd282dd9c66484a463c004d6e9f5
tspalt01 <tcs119@msn.com>
2026-05-29 22:29:24 -0700

    Added Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
